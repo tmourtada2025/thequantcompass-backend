491ad7bb3e181dc290c62a201c0afce6206f2233
tmourtada2025 <tmourtada@gmail.com>
2025-08-05 19:47:54 +0300

    metaapi_client.py

--- metaapi_client.py
+++ metaapi_client.py
@@ -1,34 +1,34 @@
-from metaapi.cloud_metaapi_sdk import MetaApi
 import os
+import asyncio
 from dotenv import load_dotenv
+from metaapi.cloud_metaapi_sdk import MetaApi
 
 load_dotenv()
 
-API_TOKEN = os.getenv("API_TOKEN")
+API_TOKEN = os.getenv("META_API_TOKEN")
+ACCOUNT_ID = os.getenv("META_API_ACCOUNT_ID")
 
 metaapi = MetaApi(API_TOKEN)
 
+
 async def fetch_prices():
     try:
-        accounts = await metaapi.metatrader_account_api.get_all_accounts()
-        if not accounts:
-            return {"error": "No MetaTrader accounts found."}
-
-        account = accounts[0]
-        await account.load()
-
-        if not account.connection_status == 'CONNECTED':
-            return {"error": f"Account {account.id} not connected"}
+        account = await metaapi.metatrader_account_api.get_account(ACCOUNT_ID)
+        await account.deploy()
+        await account.wait_connected()
 
         connection = account.get_streaming_connection()
         await connection.connect()
-        await connection.wait_synchronized(timeout_in_seconds=30)
+        await connection.wait_synchronized()
 
-        price = await connection.get_price('XAUUSD')
-        if price:
-            return {"symbol": "XAUUSD", "bid": price['bid'], "ask": price['ask']}
-        else:
-            return {"error": "Price not available for XAUUSD"}
+        # Fetch price for US30.cash (can be changed)
+        price = await connection.get_symbol_price("US30.cash")
+        return {
+            "symbol": "US30.cash",
+            "bid": price['bid'],
+            "ask": price['ask'],
+            "time": price['time']
+        }
 
     except Exception as e:
         return {"error": str(e)}
