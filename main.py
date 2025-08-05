c3f0663afada8eb5e7e76131260df5d28648c396
tmourtada2025 <tmourtada@gmail.com>
2025-08-05 20:39:10 +0300

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
-import asyncio
 from metaapi_client import fetch_prices
 
 app = FastAPI()
 
 @app.get("/prices")
-async def get_prices():
-    result = await fetch_prices()
-    return result
+async def prices():
+    return await fetch_prices()
